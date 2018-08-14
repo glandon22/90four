7ae9d2ff174707d21dd3883f597255130b8b5a39
George Wright <glandonwright@gmail.com>
2018-08-14 14:21:25 -0400

    adding sass

---
+++ scripts/chart-builder.js
@@ -0,0 +1,36 @@
+var ctx = document.getElementById("books-per-year").getContext('2d');
+
+var booksPerYear = new Chart(ctx, {
+	type: 'bar',
+    data: {
+        labels: [ '2014', '2015', '2016', '2017', '2018' ],
+        datasets: [{
+            label: 'Books',
+            data: [ 1, 11, 38, 29, 21 ],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
--- scripts/d3builder.js
+++
@@ -1,5 +0,0 @@
-var graphData = [ { year: '2014', books: 1 },
-  { year: '2015', books: 11 },
-  { year: '2016', books: 38 },
-  { year: '2017', books: 29 },
-  { year: '2018', books: 21 } ];
